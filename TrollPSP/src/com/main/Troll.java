c9141fb0b9249697e8e075845f0a4e553b11d0e3
XXnikoDarkXX <nicocheto1@gmail.com>
2020-10-04 13:46:36 +0200

    Update Troll.java

--- TrollPSP/src/com/main/Troll.java
+++ TrollPSP/src/com/main/Troll.java
@@ -1,12 +1,23 @@
 package com.main;
 
 import java.awt.AWTException;
+import java.awt.Dimension;
 import java.awt.Robot;
+import java.awt.Toolkit;
 
 public class Troll {
 
 	public static void main(String[] args) {
 	
+		//Vamos a conocer primero la resolucion de la pantalla en java para ello vamos a usar el framewoerk Toolkit
+		
+		
+		Toolkit t = Toolkit.getDefaultToolkit();
+		
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();//con la clase Dimesions conseguimos las dimensiones de
+		//cualquier componente de AWT
+		
+		
 		
 		//instanciamos la clase Robot
         Robot robot;
